bba31cadcab0b9d10bc200a8d892b95632cbd379
meghaboi <meghaboi@users.noreply.github.com>
2026-07-28 21:28:45 +0530

    Fix_CSS_and_workflow

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   basePath: isProd ? "/ascend-mun" : "",
-  assetPrefix: isProd ? "/ascend-mun/" : "",
 };
 
 export default nextConfig;
