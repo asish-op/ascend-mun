f73518ce1c89f6d39d1d16a14623d13bf7e2db86
asish <87756049+asish-op@users.noreply.github.com>
2026-09-03 18:49:44 +0530

    Update next.config.ts for production settings
    Removed eslint configuration and added basePath for production.

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
   basePath: isProd ? "/ascend-mun" : "",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
